71aecdddfe74ffe534fd018594bd427b77d300ec
Yael Yazdi <yael.yazdi96@gmail.com>
2018-11-28 22:38:21 +0200

    SEND A GIFT / MESSEGE!

--- bot.py
+++ bot.py
@@ -72,6 +72,13 @@ def send_gift(bot, update):
     custom_keyboard = [['SEND A GIFT', 'SEND A MESSAGE']]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     bot.send_message(chat_id=update.message.chat_id, text="GIFT", reply_markup=reply_markup)
+    if reply_markup == custom_keyboard[0]:
+        Choosing_gift(bot, update)
+
+def Choosing_gift(bot, update):
+    custom_keyboard = [['flowers', 'Balloons', 'Chocolates', '??']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    bot.send_message(chat_id=update.message.chat_id, text="GIFT", reply_markup=reply_markup)
 
 def help(bot, update):
     help_o = Help()
