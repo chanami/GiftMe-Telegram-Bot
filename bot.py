16a76c0ea91500087211106a26ab2c791df5de50
chanami <chanami15@gmail.com>
2018-11-28 19:43:29 +0200

    clean code after merge

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
 import datetime
-
 import secret_settings
 import settings
 import logging
-import client
 from event_model import Event
 from help import Help
 from client import Client
-
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import Updater
@@ -30,12 +27,10 @@ def start(bot, update):
     client_t = Client(settings.HOST, settings.DB)
     chat_id = update.message.chat_id
     logger.info(f"> Start chat #{chat_id}")
-
     bot.send_message(chat_id=chat_id, text="HI!!! Enter Your Full Name -- ")
     full_name = update.message.text
     status["add_member"] = 1
     client_t.create_new_member(chat_id, full_name)
-
 
 
 def respond(bot, update):
@@ -63,9 +58,10 @@ def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=message)
 
 
-def add_event(bot,update):
+def add_event(bot, update):
     global some_event
-    if(status["add_event"] == 0):
+
+    if status["add_event"] == 0:
         status["add_event"] = 4
         message = "adding event to a friend :)"
         bot.send_message(chat_id=update.message.chat_id, text=message)
@@ -73,19 +69,22 @@ def add_event(bot,update):
         bot.send_message(chat_id=update.message.chat_id, text=message)
         status["add_event"] -= 1
         some_event.append(update.message.chat_id)
-    elif(status["add_event"] == 3):
+
+    elif status["add_event"] == 3:
         name = update.message.text
         some_event.append(name)
         message = "Enter Event Type:"
         bot.send_message(chat_id=update.message.chat_id, text=message)
         status["add_event"] -= 1
-    elif (status["add_event"] == 2):
+
+    elif status["add_event"] == 2:
         type = update.message.text
         some_event.append(type)
         message = "Enter event Date <yyyy/mm/dd>: "
         bot.send_message(chat_id=update.message.chat_id, text=message)
         status["add_event"] -= 1
-    elif (status["add_event"] == 1):
+
+    elif status["add_event"] == 1:
         date = update.message.text
         date = date.split('/')
         date = [int(d) for d in date]
@@ -104,7 +103,7 @@ def show_upcoming_events(bot, update):
     message = "Upcoming Events "
     e = Event(settings.HOST, settings.TEST_DB)
     events = e.get_all_events()
-    upcoming_events=[]
+    upcoming_events = []
     for event in events:
         d0 = datetime.datetime.now()
         d1 = datetime.datetime(d0.year, event['date'].month, event['date'].day)
@@ -115,17 +114,14 @@ def show_upcoming_events(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=message)
 
 
-
-
 def show_friends(bot, update):
-    client = Client(settings.HOST, settings.DB)
-    friends = client.get_friends(update.message.chat_id)
+    c_model = Client(settings.HOST, settings.DB)
+    friends = c_model.get_friends(update.message.chat_id)
     message = "\n".join(friends)
     bot.send_message(chat_id=update.message.chat_id, text=message)
 
 
 def add_friend(bot, update):
-
     global some_friend
     if status["add_friend"] == 0:
         status["add_friend"] = 3
@@ -145,8 +141,7 @@ def add_friend(bot, update):
     elif status["add_friend"] == 1:
         address = update.message.text
         some_friend.append(address)
-        some_friend.append(False)
-
+        # some_friend.append(False)
         c = Client(settings.HOST, settings.DB)
         friend = {"full_name ": some_friend[0], "address": some_friend[1]}
         c.add_friend_to_list(update.message.chat_id, friend)
@@ -154,6 +149,7 @@ def add_friend(bot, update):
         some_friend = []
         bot.send_message(chat_id=update.message.chat_id, text=message)
         status["add_friend"] -= 1
+
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
